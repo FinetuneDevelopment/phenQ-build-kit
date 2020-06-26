691d02884a24fe7008c9c5498efcba65d8d545b2
Ross Angus <Ross-Angus@users.noreply.github.com>
2020-06-26 12:06:32 +0100

    Added accessibility JavaScript on the main naviagtion

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -4,8 +4,47 @@ export default function formFactory() {
   let myFactory = {};
 
   myFactory.init = function () {
-    //console.log("Bonk!");
-  }
+    const navCheckbox = document.getElementById('nav-toggle');
+    const navAnchors = document.querySelectorAll('.logo-nav__nav a');
+
+    window.addEventListener('resize', this.ariaUpdate);
+    navCheckbox.addEventListener('change', this.ariaUpdate);
+    this.ariaUpdate();
+
+    // Listens for navigation items falling into focus, then
+    // shows the navigation when that happens
+    for (var i = 0; i < navAnchors.length; i++) {
+      let thisAnchor = navAnchors[i];
+      thisAnchor.addEventListener('focus', function () {
+        navCheckbox.checked = true;
+      }, true);
+      thisAnchor.addEventListener('blur', function () {
+        navCheckbox.checked = false;
+      }, true);
+    }
+
+  },
+    myFactory.ariaUpdate = function () {
+      let navToggle = document.querySelector('.logo-nav__toggle label');
+      let navCheckbox = document.getElementById('nav-toggle');
+
+      // Are we in mobile view?
+      if (navToggle.offsetParent) {
+        navToggle.setAttribute('aria-haspopup', 'true');
+
+        // Is the navigation bar visible?
+        if (navCheckbox.checked) {
+          navToggle.setAttribute('aria-expanded', 'true');
+        } else {
+          navToggle.setAttribute('aria-expanded', 'false');
+        }
+      }
+      // The navigation is in desktop mode
+      else {
+        navToggle.removeAttribute('aria-haspopup');
+        navToggle.removeAttribute('aria-expanded');
+      }
+    }
 
   return myFactory;
 }
