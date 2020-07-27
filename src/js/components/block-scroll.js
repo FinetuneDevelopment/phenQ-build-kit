9894f2c907e65325a58aee34f7aa9b2b53ce2859
Ross Angus <Ross-Angus@users.noreply.github.com>
2020-07-27 10:20:45 +0100

    Made icons inherit colour better

--- src/js/components/block-scroll.js
+++ src/js/components/block-scroll.js
@@ -28,7 +28,7 @@ export default function blockScroll() {
         scrollControl.classList.add('hidden');
         scrollControl.setAttribute('data-js', 'scroll-control');
 
-        scrollControl.innerHTML = '<button class="btn btn-icon icon-x-small" type="button" data-js="previous" title="Scroll left"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" aria-label="Arrow left"><path d="M50.078 100l8.855-8.84L23.99 56.076h76.1V43.8h-76.1L58.933 8.7 50.078-.148 0 49.93z" fill="#273583"/></svg></button> <button class="btn btn-icon icon-x-small" type="button" data-js="next" title="Scroll right"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" aria-label="Arrow right"><path d="M50.003-.148l-8.855 8.84L76.1 43.775H0v12.286h76.1l-34.942 35.09L50.003 100l50.078-50.078z" fill="#273583"/></svg></button>';
+        scrollControl.innerHTML = '<button class="btn btn-icon icon-x-small" type="button" data-js="previous" title="Scroll left"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" aria-label="Arrow left"><path d="M50.078 100l8.855-8.84L23.99 56.076h76.1V43.8h-76.1L58.933 8.7 50.078-.148 0 49.93z" fill="currentColor"/></svg></button> <button class="btn btn-icon icon-x-small" type="button" data-js="next" title="Scroll right"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" aria-label="Arrow right"><path d="M50.003-.148l-8.855 8.84L76.1 43.775H0v12.286h76.1l-34.942 35.09L50.003 100l50.078-50.078z" fill="currentColor"/></svg></button>';
         // Insert the controls before the tabs
         currentScroll.insertAdjacentElement('beforebegin', scrollControl);
         // Add an attribute, so CSS can hide the scrollbar
